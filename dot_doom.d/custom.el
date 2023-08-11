44ddbfb84c11732fc9d77b1ecbad25165ac481b9
Sean Del Castillo <seanbdcastillo@gmail.com>
2023-08-11 12:49:42 -0700

    desktop doom config

---
+++ dot_doom.d/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a5270d86fac30303c5910be7403467662d7601b821af2ff0c4eb181153ebfc0a" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" default))
+ '(org-agenda-files
+   '("/home/sean/Documents/org-files/gcal.org" "/home/sean/Documents/org-files/GTD.org" "/home/sean/Documents/org-files/archive.org" "/home/sean/Documents/org-files/inbox.org" "/home/sean/Documents/org-files/someday.org"))
+ '(warning-suppress-types '((emacs) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
